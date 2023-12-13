6bab1d0193da7625834538ab5cd549e1c1ab96b8
JuZae <julian.zaehring@web.de>
2023-12-13 16:25:12 +0100

    camera works now

---
+++ src/components/CameraComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <q-btn color="primary" label="Get Picture" @click="captureImage" />
+
+    <img :src="imageSrc" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+const imageSrc = ref('');
+
+async function captureImage() {
+  const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.Uri,
+  });
+
+  // The result will vary based on the value of the resultType option.
+  // CameraResultType.Uri - Get the result from image.webPath
+  // CameraResultType.Base64 - Get the result from image.base64String
+  // CameraResultType.DataUrl - Get the result from image.dataUrl
+  imageSrc.value = image.webPath;
+}
+</script>
--- src/pages/VibrateTest.vue
+++
@@ -1,5 +0,0 @@
-<template><HapticsComponent></HapticsComponent></template>
-
-<script setup lang="ts">
-import HapticsComponent from 'src/components/HapticsComponent.vue';
-</script>
