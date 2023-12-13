490d6525a7f7923bffea19c37793d15b244aa405
JuZae <julian.zaehring@web.de>
2023-12-13 15:54:13 +0100

    test

--- src/components/HapticsComponent.vue
+++ src/components/HapticsComponent.vue
@@ -1,9 +1,19 @@
 <template>
-  <q-btn @click="hapticsVibrate()" label="Vibrate"></q-btn>
+  <q-btn
+    @click="
+      hapticsImpactMedium();
+      hapticsVibrate();
+    "
+    label="Vibrate"
+  ></q-btn>
 </template>
 
 <script setup lang="ts">
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
+
+const hapticsImpactMedium = async () => {
+  await Haptics.impact({ style: ImpactStyle.Medium });
+};
 
 const hapticsVibrate = async () => {
   await Haptics.vibrate();
